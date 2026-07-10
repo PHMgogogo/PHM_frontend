5410044e744becf17184fe6f6f1ba7406808f67d
lynx <932563835@qq.com>
2026-07-10 19:29:58 +0800

    fix: 更新任务和开放代码的代理目标地址

--- vite.config.ts
+++ vite.config.ts
@@ -66,14 +66,14 @@ export default defineConfig({
         rewrite: (path: string) => path.replace(/^\/instance/, ''),
       },
       '/task': {
-        target: 'http://localhost:8000',
+        target: 'http://192.168.31.13:8001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/task/, '/api'),
       },
       "/opencode":{
-        target: 'http://localhost:4096',
+        target: 'http://192.168.31.13:8001',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/opencode/, ''),
+        // rewrite: (path) => path.replace(/^\/opencode/, ''),
       }
     },
   },
