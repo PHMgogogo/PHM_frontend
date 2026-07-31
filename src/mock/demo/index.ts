7eca5fe5670d543d1bc53d1cf65ea6f811069f39
lynx <932563835@qq.com>
2026-07-31 15:48:06 +0800

    refactor: 删除一些不必要的git跟踪，并优化数据展示模块

--- src/mock/demo/index.ts
+++
@@ -1,8 +0,0 @@
-// Mock 数据统一导出
-export * from './planes'
-export * from './realtime'
-export * from './diagnosis'
-export * from './health'
-export * from './trend'
-export * from './prediction'
-export * from './maintenance'
